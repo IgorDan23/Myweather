aa57e286e6cba828a624f4254dd299f6560ba6eb
Igor23 <Igor_dan@inbox.ru>
2022-03-29 01:48:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myweather/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.myweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myweather.view.MainFragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState==null){
+            supportFragmentManager.beginTransaction().replace(R.id.container, MainFragment.newInstance()).commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myweather/view/MainFragment.kt
@@ -0,0 +1,53 @@
+package com.example.myweather.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.myweather.R
+import com.example.myweather.databinding.FragmentMainBinding
+import com.example.myweather.viewmodel.MainViewModel
+
+class MainFragment : Fragment() {
+
+    lateinit var binding:FragmentMainBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding= FragmentMainBinding.inflate(inflater,container,false)
+       // return inflater.inflate(R.layout.fragment_main, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val viewModel=ViewModelProvider(this).get(MainViewModel::class.java)
+        val observer = object:Observer<Any>{
+            override fun onChanged(t: Any) {
+                renderData(t)
+
+            }
+
+        }
+        viewModel.getData().observe(viewLifecycleOwner,observer)
+        viewModel.getWeather()
+
+    }
+private fun renderData(data:Any){
+    Toast.makeText(requireContext(),"Исправно",Toast.LENGTH_LONG).show()
+}
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = MainFragment()
+
+    }
+}
